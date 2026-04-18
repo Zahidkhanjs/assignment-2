686e3d85abd523c080547ee79ae23cbd9f3298a0
zahidkhanjs <zahidshahwani9925@gmail.com>
2026-04-18 05:11:14 +0500

    first commit

--- 01-javascript/01-Introduction/app.js
+++ 01-javascript/01-Introduction/app.js
@@ -2,3 +2,17 @@
 
 
 console.log("sabir khan");
+
+
+//data typs 
+//string  " zahid kahn "   " "
+//Number  1230
+ // boolean  true fals
+
+ // object 
+ // symbol 
+ // bgint 
+ // undfine
+ // null
+
+ // number 
